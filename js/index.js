520005fae2227e13ecc432b79f5748aea65a7cc6
Peyton Mathis <pmathis99@comcast.net>
2020-10-30 14:19:17 -0400

    border collisions are finished. need to test the die border rule, but for now going to move on to sprite-sprite collisions

--- js/index.js
+++ js/index.js
@@ -6,8 +6,8 @@ var scene = new Scene(canvas, 60, 0, 0, canvas.width, canvas.height);
 
 // Create blue rect sprite
 var blue_rect = new Sprite(0, 0, document.getElementById("blue_rect"), scene, BORDER_BOUNCE, true);
-blue_rect.dx = 0.2;
-blue_rect.dy = 0.1;
+blue_rect.dx = 0.65;
+blue_rect.dy = 0.3;
 
 scene.addSprite(blue_rect);
 
