e9b00270681cb9ac8303ae2712f9c314205e875b
Peyton Mathis <pmathis99@comcast.net>
2020-10-30 00:56:45 -0400

    still no picture. maybe this will work

--- js/Scene.js
+++ js/Scene.js
@@ -36,7 +36,7 @@ function Scene (canvas, frameRate, x, y, width, height) {
         
         console.log("Updated");
         // Clear the previous frame
-        this.canvas.con.clearRect(this.x, this.y, this.width, this.height);
+        //this.canvas.con.clearRect(this.x, this.y, this.width, this.height);
 
         // Call update on all current sprites
         this.sprites.forEach(function(sprite) {
@@ -46,6 +46,7 @@ function Scene (canvas, frameRate, x, y, width, height) {
         // Check if the scene is supposed to be hidden. If it is then clear the scene again
         if (this.hidden)
         {
+            console.log("i am evil");
             this.canvas.con.clearRect(this.x, this.y, this.width, this.height);
         }
     };
