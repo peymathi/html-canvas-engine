516efbe752548f7ab98d414ff76212d070ddca71
Peyton Mathis <pmathis99@comcast.net>
2020-10-30 01:40:39 -0400

    should have border collisions. still have the glitch with clearRect

--- js/index.js
+++ js/index.js
@@ -5,10 +5,10 @@ var canvas = new Canvas("mainCanvas");
 var scene = new Scene(canvas, 60, 0, 0, canvas.width - canvas.offsetx, canvas.height - canvas.offsety);
 
 // Create blue rect sprite
-var blue_rect = new Sprite(20, 20, document.getElementById("blue_rect"), scene, BORDER_BOUNCE, true);
+var blue_rect = new Sprite(0, 0, document.getElementById("blue_rect"), scene, BORDER_BOUNCE, true);
 blue_rect.dx = 0.25;
 blue_rect.dy = 0.6;
 
 scene.addSprite(blue_rect);
 scene.start();
-setTimeout(function() {scene.end()}, 15000);
+setTimeout(function() {scene.end()}, 60000);
