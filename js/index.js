f0c097138e8780940593aca2b802e426a6e94766
Peyton Mathis <pmathis99@comcast.net>
2020-10-29 23:25:11 -0400

    first test

--- js/index.js
+++ js/index.js
@@ -8,3 +8,5 @@ let scene = new Scene(canvas, 60, 0, 0, canvas.width, canvas.height);
 let blue_rect = new Sprite(20, 20, document.getElementById("blue_rect"), scene, BORDER_BOUNCE, true);
 blue_rect.dx = 0.25;
 blue_rect.dy = 0.6;
+
+scene.start();
