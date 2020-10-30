1facdc47b32ced8d2e23d94bab1e5f52002ab13d
Peyton Mathis <pmathis99@comcast.net>
2020-10-30 13:57:07 -0400

    testing high speeds

--- js/index.js
+++ js/index.js
@@ -6,13 +6,13 @@ var scene = new Scene(canvas, 60, 0, 0, canvas.width, canvas.height);
 
 // Create blue rect sprite
 var blue_rect = new Sprite(0, 0, document.getElementById("blue_rect"), scene, BORDER_BOUNCE, true);
-blue_rect.dx = 0.25;
-blue_rect.dy = 0.6;
+blue_rect.dx = 1.5;
+blue_rect.dy = 2.7;
 
 scene.addSprite(blue_rect);
 
 scene.start();
-setTimeout(function() {scene.end()}, 60000);
+setTimeout(function() {scene.end()}, 100000);
 let img = document.getElementById("blue_rect");
 
 
