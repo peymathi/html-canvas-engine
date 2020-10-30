f4cad8955f181ff70350d703b7e67e4df3a93b00
Peyton Mathis <pmathis99@comcast.net>
2020-10-30 02:20:19 -0400

    trying to fix canvas

--- js/Canvas.js
+++ js/Canvas.js
@@ -4,6 +4,8 @@ function Canvas (id) {
     
     // Holds the JavaScript Canvas element
     this.element = document.getElementById(id);
+    this.element.width = document.getElementById("container").offsetWidth - 50;
+    this.element.height = document.getElementById("container").offsetWidth - 15;
 
     // Offsets for calculating mouse position within the canvas
     this.offsety = this.element.offsetTop;
